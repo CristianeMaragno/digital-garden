4519831456267b2bd299782a99bec45c5f3ae829
Cristiane R. Maragno <cristianerodriguesmaragno@gmail.com>
2023-08-11 17:09:18 -0300

    Add home page structure

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -3,7 +3,13 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const postsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+      take: 20,
+      where: {
+        home: true
+      }
+    });
+    return posts;
   }),
 });
